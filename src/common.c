4b459b578c3d922ddf6a015a11dfde7422a4b9c3
switchcopa <100treyholds@gmail.com>
2026-07-05 00:55:35 +0100

    implement common.c

---
+++ src/common.c
@@ -0,0 +1,18 @@
+/* common.c */
+
+#include "common.h"
+
+void
+compiler_internal_abort(const char *file, int line, const char *fmt, ...)
+{
+    va_list args;
+
+    va_start(args, fmt);
+    fprintf(stderr, "internal compiler error:\n");
+    fprintf(stderr, "%s: %i: ", file, line);
+    vfprintf(stderr, fmt, args);
+    fprintf(stderr, "\n");
+    va_end(args);
+
+    exit(EXIT_FAILURE);
+}
